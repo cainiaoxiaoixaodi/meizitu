f5ce99cac7e04926cf3a7470c6cf298db50d83c5
wc <13581470990@163.com>
2018-09-26 15:50:40 +0800

    通过bs4得到套图页url和名字

--- meizi/meizi/spiders/mz.py
+++ meizi/meizi/spiders/mz.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from pprint import pprint
+from bs4 import BeautifulSoup
 
 import scrapy
 
@@ -10,7 +11,15 @@ class MzSpider(scrapy.Spider):
     start_urls = ['http://www.meizitu.com/a/more_1.html']
 
     def parse(self, response):
-        data = response.text
-        item = {}
-        item['data'] = data
-        return item
+        item = {}  # 构造字典
+        # 初始化bs4
+        data = BeautifulSoup(response.text, 'lxml')
+        # 取出每个带有套图页url的div标签
+        div_list = data.select('ul > li.wp-item > div')
+        for div in div_list:
+            # 每个套图的url
+            map_url = div.div.a['href']
+            # 每个套图的名字
+            map_name = div.h3.a.b.text
+            print(map_url, map_name)
+            break
